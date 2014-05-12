2dcc7c51e20a3ec7affa647b20f50c7371c14e99
Antoni Batchelli <tbatchelli@acm.org>
2014-05-12 15:09:25 -0700

    Update project group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hyde "0.1.0-SNAPSHOT"
+(defproject com.palletops/hyde "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
